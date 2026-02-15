8be5e91f908f262722945c9efb2ea8599bdaf353
Nick Williams <git@nickawilliams.com>
2026-02-14 20:27:25 -0800

    chore: remove legacy bash tooling

--- package.sh
+++
@@ -1,7 +0,0 @@
-name="@rogwilco/shedoc"
-version="1.0.0"
-dependencies=(
-    "https://github.com/sstephenson/bats"
-    "https://github.com/ztombol/bats-assert"
-    "https://github.com/ztombol/bats-support"
-)
